2fac05751aa5345dd2775c802fb4ebbaa9e4c79f
davisuga <daviciencia1@gmail.com>
2023-04-18 16:22:10 -0300

    separate function for json decoder

---
+++ curriculum_gen/src/main/scala/generated/Decode.scala
@@ -0,0 +1,7 @@
+package generated_types;
+import io.circe._
+import io.circe.generic.auto._
+import io.circe.parser._
+import io.circe.syntax._
+
+val decodeRoot = decode[generated_types.RootInterface]
